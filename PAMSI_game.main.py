35a14b59a1ed4cebe2d544ff41add6b5661d078f
abdarum <kornel@KornelLaptop.MyHomeServer.com>
2018-05-28 12:47:03 +0200

    Evaluate function and after succesfull namespace change

--- PAMSI_game.main.py
+++ PAMSI_game.main.py
@@ -10,7 +10,7 @@ import pygame
 
 __author__ = 'Kornel Stefańczykr'
 __license__ = 'CC BY-SA'
-__version__ = '0.2'
+__version__ = '0.4'
 __maintainer__ = 'Kornel Stefańczyk'
 __email__ = 'kornelstanczyk@wp.pl'
 
@@ -25,9 +25,12 @@ def debug(msg, active_debugging=True, end_=None):
             
 
 class GameAI:
-    def __init__(self, current_board=None):
-        self.current_board=current_board
-
+    def __init__(self, current_data=None, max_player=None, depth=None):
+        self.current_data=current_data
+        if max_player: self.max_player = max_player
+        else:          self.max_player = self.current_data.ai_player
+        if depth: self.depth = depth
+        else:     self.depth = 0
 
     def minmax(self, scores, depth=None, node_index=None, maximalizer=None, 
             h=None):
@@ -42,7 +45,7 @@ class GameAI:
         if scores.__class__.__name__ != 'list':
             raise Exception('scores is not list')
         if depth == None:
-            depth = 0
+            depth = self.depth
         if node_index == None:
             node_index = 0
         if maximalizer == None:
@@ -65,35 +68,79 @@ class GameAI:
 
     def evaluate(self, board=None):
         if board == None:
-            board = self. current_board
-        print(board.__class__.__name__)
-                
+            board = self.current_data.matrix
+        winner = self.current_data.detect_win(board)
+        if winner:
+            if winner == self.max_player:
+                return 10
+            else:
+                return -10
+        else:
+            return 0
+
 
 class GameBasicData:
     def __init__(self):
-        self.number_columns, self.number_rows = 4, 4 
+        self.number_columns, self.number_rows = 3, 3 
         self.combo_length = 3
-        self.game_matrix = [[None for i in range(self.number_columns)]\
+        self.matrix = [[None for i in range(self.number_columns)]\
                 for j in range(self.number_rows)]
         self.ai_player = None
         self.current_player = 'X'
         self.winner = None 
 
+    def detect_win(self, board=None):
+        """Detect if user won this round
+        
+        Return name of winner"""
+        if board == None:
+            board = self.matrix
+        list_elements_to_check = []
+        #detect from top left corner to bottom right corner
+        for i in range(0, self.number_rows-self.combo_length+1):
+            for j in range(0, self.number_columns-self.combo_length+1):
+                for k in range(0, self.combo_length):
+                    list_elements_to_check.append(board[i+k][j+k])
+                if all(i == list_elements_to_check[0] and i != None\
+                        for i in list_elements_to_check):
+                    return list_elements_to_check[0]
+                list_elements_to_check = []
 
+        #detect from top right corner to bottom left corner
+        for i in range(0, self.number_rows-self.combo_length+1):
+            for j in range(self.combo_length-1, self.number_columns):
+                for k in range(0, self.combo_length):
+                    list_elements_to_check.append(board[i+k][j-k])
+                if all(i == list_elements_to_check[0] and i != None\
+                        for i in list_elements_to_check):
+                    return list_elements_to_check[0]
+                list_elements_to_check = []
+
+        #detect horizontal 
+        for i in range(0, self.number_rows):
+            for j in range(0, self.number_columns-self.combo_length+1):
+                for k in range(0, self.combo_length):
+                    list_elements_to_check.append(board[i][j+k])
+                if all(i == list_elements_to_check[0] and i != None\
+                        for i in list_elements_to_check):
+                    return list_elements_to_check[0]
+                list_elements_to_check = []
+
+        #detect vertical 
+        for i in range(0, self.number_rows-self.combo_length+1):
+            for j in range(0, self.number_columns):
+                for k in range(0, self.combo_length):
+                    list_elements_to_check.append(board[i+k][j])
+                if all(i == list_elements_to_check[0] and i != None\
+                        for i in list_elements_to_check):
+                    return list_elements_to_check[0]
+                list_elements_to_check = []
 
 
 class TicTacToe:
     def __init__(self):
 
-        self.number_columns, self.number_rows = 4, 4 
-        self.combo_length = 3
-        self.game_matrix = [[None for i in range(self.number_columns)]\
-                for j in range(self.number_rows)]
-        self.ai_player = None
-        self.current_player = 'X'
-        self.winner = None 
-
-
+        self.game = GameBasicData()
 
         pygame.init()
         self.game_active = True
@@ -106,53 +153,52 @@ class TicTacToe:
         self.color_button = 51, 51, 51
         self.color_button_font =  255, 255, 255
         self.line_size = 4
-        self.pos_dim = self.width/self.number_columns, self.height/self.number_rows
+        self.pos_dim = self.width/self.game.number_columns, self.height/self.game.number_rows
 
         self.X = pygame.image.load(os.path.join('game','img','X.png'))
         self.X = pygame.transform.scale(self.X, 
-                (self.width//self.number_columns, self.height//self.number_rows))
+                (self.width//self.game.number_columns, self.height//self.game.number_rows))
         self.Xrect = self.X.get_rect()
         self.X_list = []
         self.O = pygame.image.load(os.path.join('game','img','O.png'))
         self.O = pygame.transform.scale(self.O, 
-                (self.width//self.number_columns, self.height//self.number_rows))
+                (self.width//self.game.number_columns, self.height//self.game.number_rows))
         self.Orect = self.O.get_rect()
         self.O_list = []
  
-
     def clear_data(self):
         """Clear current game data"""
-        self.game_matrix = [[None for i in range(self.number_columns)]\
-                for j in range(self.number_rows)]
+        self.game.matrix = [[None for i in range(self.game.number_columns)]\
+                for j in range(self.game.number_rows)]
         self.X_list = []
         self.O_list = []
-        self.current_player = 'X'
-        self.winner = None
+        self.game.current_player = 'X'
+        self.game.winner = None
 
     def draw(self, type_draw='last_element'):
         """Drawing grid and newly added elements"""
         if type_draw == 'basic':
             self.screen.fill(self.color_background)
-            for i in range(1, self.number_columns):
+            for i in range(1, self.game.number_columns):
                 pygame.draw.line(self.screen, self.color_line,
                         [i*self.pos_dim[0], 0],
                         [i*self.pos_dim[0], self.height],
                         self.line_size)
-            for i in range(1, self.number_rows):
+            for i in range(1, self.game.number_rows):
                 pygame.draw.line(self.screen, self.color_line,
                         [0, i*self.pos_dim[1]],
                         [self.width, i*self.pos_dim[1]],
                         self.line_size)
         elif type_draw == 'last_element':
-            if self.current_player == 'X':
+            if self.game.current_player == 'X':
                 self.screen.blit(self.X, self.X_list[-1])
-            if self.current_player == 'O':
+            if self.game.current_player == 'O':
                 self.screen.blit(self.O, self.O_list[-1])
 
     def add_choose(self, column, row, player):
         """Adding player choice"""
         disp_pos = column*self.pos_dim[0], row*self.pos_dim[1]
-        self.game_matrix[row][column] = player
+        self.game.matrix[row][column] = player
         if player == 'X':
             self.X_list.append(self.Xrect.copy())
             self.X_list[-1] = self.X_list[-1].move(disp_pos[0],
@@ -165,23 +211,23 @@ class TicTacToe:
                     disp_pos[1])
             self.screen_redraw = True
             self.draw()
-        debug(self.game_matrix)
+        debug(self.game.matrix)
 
     def position_choose(self, position):
         """Detecting where user clicked mouse on screen and adding elent to lists"""
         tab_pos = math.floor(position[1]/self.pos_dim[1])\
                 , math.floor(position[0]/self.pos_dim[0])
         debug('Position on screen '+str(position)+', pos in game '+str(tab_pos)) 
-        if self.game_matrix[tab_pos[0]][tab_pos[1]] is None:
-            self.add_choose(tab_pos[1], tab_pos[0], self.current_player) 
-            if self.current_player == 'X':
-                self.current_player = 'O'
-            elif self.current_player == 'O':
-                self.current_player = 'X'
+        if self.game.matrix[tab_pos[0]][tab_pos[1]] is None:
+            self.add_choose(tab_pos[1], tab_pos[0], self.game.current_player) 
+            if self.game.current_player == 'X':
+                self.game.current_player = 'O'
+            elif self.game.current_player == 'O':
+                self.game.current_player = 'X'
         else:
             print('This position is not empty! It was checked by '+
-                    str(self.game_matrix[tab_pos[0]][tab_pos[1]])+
-                    '. Current player round '+ self.current_player)
+                    str(self.game.matrix[tab_pos[0]][tab_pos[1]])+
+                    '. Current player round '+ self.game.current_player)
 
     def handle_events(self):
         """Handle quit and mouse click"""
@@ -190,13 +236,13 @@ class TicTacToe:
                 self.game_active = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 position = pygame.mouse.get_pos()
-                if self.winner is None:
+                if self.game.winner is None:
                     debug('Pressed mouse'+str(position))
                     self.position_choose(position)
-                    self.winner = self.detect_win()
+                    self.game.winner = self.game.detect_win()
                     self.detect_full_board()
-                    if self.winner:
-                        print('\''+self.winner+'\' won this round')
+                    if self.game.winner:
+                        print('\''+self.game.winner+'\' won this round')
                         self.button()
                 else:
                     self.button(position)
@@ -226,71 +272,25 @@ class TicTacToe:
 
     def detect_full_board(self):
         """Return True if board is full"""
-        for i in range(0, self.number_rows):
-            for j in range(0, self.number_columns):
-                if self.game_matrix[i][j] is None:
+        for i in range(0, self.game.number_rows):
+            for j in range(0, self.game.number_columns):
+                if self.game.matrix[i][j] is None:
                     return False
-        self.winner = 'full_board'
+        self.game.winner = 'full_board'
         return True
 
-    def detect_win(self):
-        """Detect if user won this round
-        
-        Return name of winner"""
-        list_elements_to_check = []
-        #detect from top left corner to bottom right corner
-        for i in range(0, self.number_rows-self.combo_length+1):
-            for j in range(0, self.number_columns-self.combo_length+1):
-                for k in range(0, self.combo_length):
-                    list_elements_to_check.append(self.game_matrix[i+k][j+k])
-                if all(i == list_elements_to_check[0] and i != None\
-                        for i in list_elements_to_check):
-                    return list_elements_to_check[0]
-                list_elements_to_check = []
-
-        #detect from top right corner to bottom left corner
-        for i in range(0, self.number_rows-self.combo_length+1):
-            for j in range(self.combo_length-1, self.number_columns):
-                for k in range(0, self.combo_length):
-                    list_elements_to_check.append(self.game_matrix[i+k][j-k])
-                if all(i == list_elements_to_check[0] and i != None\
-                        for i in list_elements_to_check):
-                    return list_elements_to_check[0]
-                list_elements_to_check = []
-
-        #detect horizontal 
-        for i in range(0, self.number_rows):
-            for j in range(0, self.number_columns-self.combo_length+1):
-                for k in range(0, self.combo_length):
-                    list_elements_to_check.append(self.game_matrix[i][j+k])
-                if all(i == list_elements_to_check[0] and i != None\
-                        for i in list_elements_to_check):
-                    return list_elements_to_check[0]
-                list_elements_to_check = []
-
-        #detect vertical 
-        for i in range(0, self.number_rows-self.combo_length+1):
-            for j in range(0, self.number_columns):
-                for k in range(0, self.combo_length):
-                    list_elements_to_check.append(self.game_matrix[i+k][j])
-                if all(i == list_elements_to_check[0] and i != None\
-                        for i in list_elements_to_check):
-                    return list_elements_to_check[0]
-                list_elements_to_check = []
-
     def ai_opponent(self):
-        if self.ai_player:
-            if self.current_player == self.ai_player:
+        """Handle mode player vs AI"""
+        if self.game.ai_player:
+            if self.game.current_player == self.game.ai_player:
                 pass
 
-        AI = GameAI(self.game_matrix)
-        AI.evaluate()
-
-
+        AI = GameAI(self.game)
+        print(AI.evaluate())
 
     def play(self):
         """Join all elements and do most of drawing"""
-        debug(self.game_matrix)
+        debug(self.game.matrix)
         self.draw('basic')
         pygame.display.update()
         while self.game_active:
@@ -300,7 +300,7 @@ class TicTacToe:
             if self.screen_redraw:
                 pygame.display.update()
                 self.screen_redraw = False
-            pygame.time.wait(500)
+            pygame.time.wait(50)
 
 
 Game = TicTacToe()
